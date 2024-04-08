e13254d13925c1dcd81b22c10a716e39d209e34f
D'Angelo <dangelokwakye@gmail.com>
2024-04-08 00:26:21 +0000

    firebase integration

---
+++ src/config/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGsZ1VJOj89ZWPiqal2NeCxTXIVAmV69I",
+  authDomain: "jamalia-6f98a.firebaseapp.com",
+  projectId: "jamalia-6f98a",
+  storageBucket: "jamalia-6f98a.appspot.com",
+  messagingSenderId: "381035563936",
+  appId: "1:381035563936:web:8e83731cf80bf619afb2c5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
--- src/pages/signup-page.jsx
+++ src/pages/signup-page.jsx
@@ -1,7 +1,108 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import "../../src/index.css";
+import { auth, db } from "../config/firebase";
+import { addDoc, collection } from "firebase/firestore";
+
 export default function SignUp() {
-    return(
-        <>
-        <p>Sign up page goes here</p>
-        </>
+  const signin = (event) => {
+    event.preventDefault();
+    const signupData = new FormData(event.target);
+    // call firebase Auth signup
+    createUserWithEmailAndPassword(
+      auth,
+      signupData.get("email"),
+      signupData.get("password")
     )
-};
+      .then(async (userCredential) => {
+        // create user in firestore
+        try {
+            const docRef = await addDoc(collection(db, "users"), {
+              userId: userCredential.user.uid,
+              conpanyName: signupData.get('companyName'),
+              address: signupData.get('address'),
+              contactDetails: signupData.get('contactDetails')
+            });
+            console.log("Document written with ID: ", docRef.id);
+          } catch (e) {
+            console.error("Error adding document: ", e);
+          }
+          
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <div className="section__container">
+        <section className="signup">
+          <div className="signup__container">
+            <p>Jamalia</p>
+            <form onSubmit={signin} className="form">
+              <div>
+                <label htmlFor="title">Email Address*</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className=""
+                  placeholder="Enter your email address"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="title">Password*</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  className=""
+                  placeholder="Enter your passowrd"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="title">Company Name*</label>
+                <input
+                  type="text"
+                  id="companyname"
+                  name="companyName"
+                  className=""
+                  placeholder="Enter your company name"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="title">Address*</label>
+                <textarea
+                  type="text"
+                  id="address"
+                  name="address"
+                  className=""
+                  placeholder="Enter your company Address"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="title">Contact details*</label>
+                <input
+                  type="text"
+                  id="contactdetails"
+                  name="contactDetails"
+                  className=""
+                  placeholder="Eg. +233 ** *** ****"
+                />
+              </div>
+
+              <button type="submit" className="submitBtn">
+                Sign Up
+              </button>
+            </form>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
