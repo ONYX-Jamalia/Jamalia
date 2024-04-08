63a0d1e4ca6921691db40b485b4868b3d634f9fe
AngeloKwakye <dangelokwakye@gmail.com>
2024-04-08 17:37:43 +0000

    notifications

--- src/pages/signup-page.jsx
+++ src/pages/signup-page.jsx
@@ -3,9 +3,12 @@ import "../../src/index.css";
 import { auth, db } from "../config/firebase";
 import { addDoc, collection } from "firebase/firestore";
 import { useNavigate } from 'react-router-dom';
+import { useState } from "react";
 
 export default function SignUp() {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false)
+  const [errorMessage, setErrorMessage] = useState(false);
 
   const signUp = (event) => {
     event.preventDefault();
@@ -29,19 +32,23 @@ export default function SignUp() {
           } catch (e) {
             console.error("Error adding document: ", e);
           }
-          
-        navigate('/');
-        console.log(userCredential);
+          setLoading(true);
+        setTimeout(()=>{
+          navigate('/');
+          console.log(userCredential);
+        }, 2000)  
+       
       })
       .catch((error) => {
+        setErrorMessage(true)
         console.log(error);
-      });
+      }).finally(()=> setLoading(false));
   };
 
   return (
     <>
       <div className="section__container">
-        <section className="signup">
+        {!loading ? <section className="signup">
           <div className="signup__container">
             <p>Jamalia</p>
             <form onSubmit={signUp} className="form">
@@ -105,7 +112,22 @@ export default function SignUp() {
               </button>
             </form>
           </div>
-        </section>
+        </section> : <div class="p-4 mb-4 text-sm text-green-800 rounded-lg bg-green-50 dark:bg-gray-800 dark:text-green-400" role="alert">
+  <span class="font-medium">Success!</span> Sign in successfull.
+</div> ? errorMessage : 
+  <div class="flex items-center p-4 mb-4 text-sm text-red-800 rounded-lg bg-red-50 dark:bg-gray-800 dark:text-red-400" role="alert">
+  <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+  </svg>
+  <span class="sr-only">Info</span>
+  <div>
+    <span class="font-medium">Danger alert!</span> Change a few things up and try submitting again.
+  </div>
+</div>
+}
+
+        
+
       </div>
     </>
   );
